728e58c51d0f886cc57ff9e42d46591679f9923f
Vince R <r2d2fan20@outlook.com>
2021-04-15 23:52:42 -0500

    Init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Header from "./shared/Header.svelte";
+	import Content from "./shared/Content.svelte";
+</script>
+
+<Header>W.I.S. Support</Header>
+
+<Content>
+	<p>Hello, Human! Expirencing problems with W.I.S. or its surrounding services? Not to worry, I've got your back.</p>
+	<p>Fill in basic information about you problem, your email, and a detailed description of your issue and i'll email you back ASAP</p>
+</Content>
---
+++ src/shared/Header.svelte
@@ -0,0 +1,32 @@
+<div class="header"><slot></slot></div>
+<div class="wave"></div>
+
+<style>
+    .header {
+        position: absolute;
+        width: 100%;
+        height: 60%;
+        top: 0;
+        left: 0;
+        background-image: url("Background.png");
+        display: flex;
+        justify-content: center;
+        font-size: 2rem;
+        color: #fff;
+        padding: 1em;
+        box-sizing: border-box;
+    }
+
+    .wave {
+        position: absolute;
+        top: 1px;
+        left: 0;
+        width: 100%;
+        height: 60%;
+        background-image: url("wave.svg");
+        background-position-x: center;
+        background-position-y: bottom;
+        background-size: cover;
+
+    }
+</style>
